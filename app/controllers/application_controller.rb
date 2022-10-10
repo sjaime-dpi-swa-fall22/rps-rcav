aabe85b192deec7d8bfd0d52008e0b351c5ad573
sjaime-dpi <sjaime@uillinois.edu>
2022-10-10 01:24:44 +0000

    completed rps rcav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,60 @@ class ApplicationController < ActionController::Base
   # ================================
 
   def play_rock
-    render({ :template => "game_templates/rock.html.erb"})
+    index = rand(3)
+    moves = ["rock", "paper", "scissors"]
+    @comp_move=moves[index]
 
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    elsif @comp_move == "scissors"
+      @outcome = "won"
+    end
+
+    #redirect_to("https://www.google.com")  # to redirect to a different URL
+    #render({ :plain => "Hello world!"}) # to render plain text
+    # render({ :html => "<h1>Hello, World!</h1>".html_safe }) # to render html #directly, ,html_sage is required for security reasons
+
+    render({ :template => "game_templates/rock.html.erb"})
+  end
+
+  def play_paper
+    index = rand(3)
+    moves = ["rock", "paper", "scissors"]
+    @comp_move=moves[index]
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    end
+
+    render({ :template => "game_templates/paper.html.erb"})
+  end
+
+  def play_scissors
+    index = rand(3)
+    moves = ["rock", "paper", "scissors"]
+    @comp_move=moves[index]
+
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @outcome = "tied"
+    end
+
+    render({ :template => "game_templates/scissors.html.erb"})
+  end
+
+  def homepage
+
+   render({ :template => "index.html.erb"})
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 
   get("/rock", { :controller => "application", :action => "play_rock"}) # when someone visits /rock, execute a Ruby method called play_rock
+  get("/paper", { :controller => "application", :action => "play_paper"}) # when someone visits /paper, execute a Ruby method called play_paper
+  get("/scissors", { :controller => "application", :action => "play_scissors"}) # when someone visits /scissors, execute a Ruby method called play_scissors
+  get("/", { :controller => "application", :action => "homepage"})
 end
